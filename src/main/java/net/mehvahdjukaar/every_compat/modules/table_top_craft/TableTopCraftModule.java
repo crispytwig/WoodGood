b6becc83cdebea87bd8942e571ded76b39da2799
Xelbayria <12866666+Xelbayria@users.noreply.github.com>
2024-10-16 18:10:58 -0600

    TableTopCraftModule: Updated

--- src/main/java/net/mehvahdjukaar/every_compat/modules/table_top_craft/TableTopCraftModule.java
+++ src/main/java/net/mehvahdjukaar/every_compat/modules/table_top_craft/TableTopCraftModule.java
@@ -1,27 +1,30 @@
 package net.mehvahdjukaar.every_compat.modules.table_top_craft;
 
 import andrews.table_top_craft.TableTopCraft;
+import andrews.table_top_craft.objects.blocks.ChessBlock;
 import andrews.table_top_craft.tile_entities.ChessTileEntity;
 import andrews.table_top_craft.tile_entities.render.ChessTileEntityRenderer;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
 import net.mehvahdjukaar.selene.block_set.BlockType;
 import net.mehvahdjukaar.selene.block_set.wood.WoodType;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.registries.IForgeRegistry;
+import org.jetbrains.annotations.NotNull;
 
 //SUPPORT: FORGE-v2.2.0+
 public class TableTopCraftModule extends SimpleModule {
-    // Blocks
+
     public final SimpleEntrySet<WoodType, Block> CHESS_BOARDS;
-    // Block Entity Types
-    public static BlockEntityType<? extends ChessTileEntity> CHESS_TILE;
 
     public TableTopCraftModule(String modId) {
         super(modId, "ttc");
@@ -52,14 +55,36 @@ public class TableTopCraftModule extends SimpleModule {
     }
 
     @Override
-    public void registerTiles(IForgeRegistry<BlockEntityType<?>> registry) {
-        super.registerTiles(registry);
-        CHESS_TILE = (BlockEntityType<? extends ChessTileEntity>) CHESS_BOARDS.getTileHolder().tile;
-    }
-
-    @Override
+    @SuppressWarnings({"unchecked", "DataFlowIssue"})
     public void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
         super.registerEntityRenderers(event);
-        event.registerBlockEntityRenderer(CHESS_TILE, ChessTileEntityRenderer::new);
+        event.registerBlockEntityRenderer((BlockEntityType<? extends ChessTileEntity>) CHESS_BOARDS.getTileHolder().get(),
+                ChessTileEntityRenderer::new);
+    }
+
+// CLASSES
+    public class CompatChessBlock extends ChessBlock {
+
+        public CompatChessBlock(Material material, SoundType soundType) {
+            super(material, soundType);
+        }
+
+        @Override
+        public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+            return new CompatChessBlockTile(pos, state);
+        }
+    }
+
+    public class CompatChessBlockTile extends ChessTileEntity {
+
+        public CompatChessBlockTile(BlockPos pos, BlockState state) {
+            super(pos, state);
+        }
+
+        @Override
+        @SuppressWarnings("DataFlowIssue")
+        public @NotNull BlockEntityType<?> getType() {
+            return CHESS_BOARDS.getTileHolder().get();
+        }
     }
 }
