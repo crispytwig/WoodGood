d5180ac045384263af767377038a41adf5805cbc
Xelbayria <12866666+Xelbayria@users.noreply.github.com>
2024-10-22 00:15:37 -0600

    WoodworksModule: Added #minecraft:mineable/axe to ladders

--- neoforge/src/main/java/net/mehvahdjukaar/every_compat/modules/neoforge/abnormal/WoodworksModule.java
+++ neoforge/src/main/java/net/mehvahdjukaar/every_compat/modules/neoforge/abnormal/WoodworksModule.java
@@ -109,6 +109,7 @@ public class WoodworksModule extends SimpleModule {
                         () -> WoodTypeRegistry.getValue(ResourceLocation.parse("spruce")),
                         w -> new LadderBlock(WoodworksBlocks.WoodworksProperties.SPRUCE_WOOD.ladder()))
                 .setTabKey(tab)
+                .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                 .addTag(BlockTags.CLIMBABLE, Registries.BLOCK)
                 .addTag(ResourceLocation.parse("quark:ladders"), Registries.BLOCK)
                 .addTag(ResourceLocation.parse("quark:ladders"), Registries.ITEM)
