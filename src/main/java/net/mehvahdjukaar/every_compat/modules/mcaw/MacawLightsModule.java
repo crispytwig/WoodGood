b5724cc83042392a83156250e475f92c14e00092
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-11-19 02:14:25 -0700

    MacawLightsModule: Added ceiling_fan_light

--- src/main/java/net/mehvahdjukaar/every_compat/modules/mcaw/MacawLightsModule.java
+++ src/main/java/net/mehvahdjukaar/every_compat/modules/mcaw/MacawLightsModule.java
@@ -2,114 +2,90 @@ package net.mehvahdjukaar.every_compat.modules.mcaw;
 
 import com.mcwlights.kikoz.MacawsLights;
 import com.mcwlights.kikoz.init.BlockInit;
+import com.mcwlights.kikoz.objects.LightBaseShort;
 import com.mcwlights.kikoz.objects.TikiTorch;
 import net.mehvahdjukaar.every_compat.WoodGood;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
-import net.mehvahdjukaar.every_compat.dynamicpack.ClientDynamicResourcesHandler;
-import net.mehvahdjukaar.every_compat.misc.Utils;
-import net.mehvahdjukaar.every_compat.modules.quark.CompatChestBlock;
 import net.mehvahdjukaar.selene.block_set.wood.WoodType;
-import net.mehvahdjukaar.selene.client.asset_generators.textures.Palette;
-import net.mehvahdjukaar.selene.client.asset_generators.textures.Respriter;
-import net.mehvahdjukaar.selene.client.asset_generators.textures.SpriteUtils;
-import net.mehvahdjukaar.selene.client.asset_generators.textures.TextureImage;
-import net.mehvahdjukaar.selene.math.colors.HCLColor;
-import net.mehvahdjukaar.selene.resourcepack.RPUtils;
-import net.mehvahdjukaar.selene.resourcepack.ResType;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.Registry;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.common.crafting.conditions.ICondition;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.function.ToIntFunction;
 
-//SUPPORT: v1.0.6+
+//SUPPORT: v1.1.0+
 public class MacawLightsModule extends SimpleModule {
 
-    public final SimpleEntrySet<WoodType, Block> SOUL_TIKI_TORCHES;
-    public final SimpleEntrySet<WoodType, Block> TIKI_TORCHES;
+    public final SimpleEntrySet<WoodType, Block> soul_tiki_torches;
+    public final SimpleEntrySet<WoodType, Block> tiki_torches;
+    public final SimpleEntrySet<WoodType, Block> ceiling_fans;
 
     public MacawLightsModule(String modId) {
         super(modId, "mcl");
+        CreativeModeTab tab = MacawsLights.LightsItemGroup;
 
-        CreativeModeTab tab = null;
-
-        SOUL_TIKI_TORCHES = SimpleEntrySet.builder(WoodType.class, "tiki_torch", "soul",
+        soul_tiki_torches = SimpleEntrySet.builder(WoodType.class, "tiki_torch", "soul",
                         BlockInit.SOUL_OAK_TIKI_TORCH, () -> WoodType.OAK_WOOD_TYPE,
-                        w -> new TikiTorch(WoodGood.copySafe(w.planks).strength(0.2f, 2.5f), ParticleTypes.SOUL_FIRE_FLAME))
+                        w -> new TikiTorch(copyProperties(15), ParticleTypes.SOUL_FIRE_FLAME)
+                )
+                //TEXTURES: logs
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registry.BLOCK_REGISTRY)
                 .setRenderType(() -> RenderType::cutout)
                 .defaultRecipe()
                 .setTab(() -> tab)
                 .build();
+        this.addEntry(soul_tiki_torches);
 
-        this.addEntry(SOUL_TIKI_TORCHES);
 
-        TIKI_TORCHES = SimpleEntrySet.builder(WoodType.class, "tiki_torch",
+        tiki_torches = SimpleEntrySet.builder(WoodType.class, "tiki_torch",
                         BlockInit.OAK_TIKI_TORCH, () -> WoodType.OAK_WOOD_TYPE,
-                        w -> new TikiTorch(WoodGood.copySafe(w.planks).strength(0.2f, 2.5f), ParticleTypes.FLAME))
+                        w -> new TikiTorch(copyProperties(16), ParticleTypes.FLAME)
+                )
+                //TEXTURES: logs
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registry.BLOCK_REGISTRY)
                 .setRenderType(() -> RenderType::cutout)
                 .defaultRecipe()
                 .setTab(() -> tab)
                 .build();
+        this.addEntry(tiki_torches);
 
-        this.addEntry(TIKI_TORCHES);
+        ceiling_fans = SimpleEntrySet.builder(WoodType.class, "ceiling_fan_light",
+                        BlockInit.OAK_CEILING_FAN_LIGHT, () -> WoodType.OAK_WOOD_TYPE,
+                        w -> new LightBaseShort(copyProperties(15))
+                )
+                .addTextureM(modRes("block/oak_ceiling_fan"),
+                        WoodGood.res("block/mcaw/lights/oak_ceiling_fan_m"))
+                .addTag(BlockTags.MINEABLE_WITH_AXE, Registry.BLOCK_REGISTRY)
+                .setRenderType(() -> RenderType::cutout)
+                .defaultRecipe()
+                .setTab(() -> tab)
+                .build();
+        this.addEntry(ceiling_fans);
+
     }
 
-
-    @Override
-    public void addDynamicClientResources(ClientDynamicResourcesHandler handler, ResourceManager manager) {
-        super.addDynamicClientResources(handler, manager);
-
-
-        try (TextureImage mask = TextureImage.open(manager, WoodGood.res("item/tiki_torch_mask"));
-             TextureImage overlay_soul = TextureImage.open(manager, WoodGood.res("item/tiki_torch_overlay"));
-             TextureImage overlay = TextureImage.open(manager, WoodGood.res("item/tiki_torch_soul_overlay"))
-        ) {
-
-
-            TIKI_TORCHES.blocks.forEach((wood, block) -> {
-                var id = block.getRegistryName();
-
-                try (TextureImage logTexture = TextureImage.open(manager,
-                        RPUtils.findFirstBlockTextureLocation(manager, wood.log, SpriteUtils::looksLikeSideLogTexture))) {
-
-                    var t = mask.makeCopy();
-                    t.applyOverlayOnExisting(logTexture.makeCopy(),overlay.makeCopy());
-
-                    handler.dynamicPack.addAndCloseTexture(new ResourceLocation(id.getNamespace(),
-                            "item/"+ id.getPath().replace("_torch","")), t);
-
-                } catch (Exception ignored) {
-                }
-            });
-            SOUL_TIKI_TORCHES.blocks.forEach((wood, block) -> {
-                var id = block.getRegistryName();
-
-                try (TextureImage logTexture = TextureImage.open(manager,
-                        RPUtils.findFirstBlockTextureLocation(manager, wood.log, SpriteUtils::looksLikeSideLogTexture))) {
-
-                    var t = mask.makeCopy();
-                    t.applyOverlayOnExisting(logTexture.makeCopy(),overlay_soul.makeCopy());
-
-                    handler.dynamicPack.addAndCloseTexture(new ResourceLocation(id.getNamespace(),
-                            "item/"+ id.getPath().replace("_torch","")), t);
-
-                } catch (Exception ignored) {
-                }
-            });
-        } catch (Exception ex) {
-            handler.getLogger().error("Could not generate any Tiki torch item texture : ", ex);
-        }
+    @SuppressWarnings("SameParameterValue")
+    private BlockBehaviour.Properties copyProperties(int num) {
+        return BlockBehaviour.Properties.of(Material.WOOD)
+                .color(MaterialColor.WOOD)
+                .strength(0.2f, 2.5f)
+                .lightLevel(blockOffLightValue(num))
+                .sound(SoundType.WOOD)
+                .noOcclusion();
     }
+
+    private static ToIntFunction<BlockState> blockOffLightValue(int num) {
+        return (state) -> (Boolean)state.getValue(BlockStateProperties.LIT) ? num : 0;
+    }
+
 }
