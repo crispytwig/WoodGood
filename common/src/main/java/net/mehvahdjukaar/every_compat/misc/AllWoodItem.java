8652b87a61e7c0ba89af97e61dd8c895298cba49
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-12-04 14:09:11 +0100

    generalized all wood item

--- common/src/main/java/net/mehvahdjukaar/every_compat/misc/AllWoodItem.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/misc/AllWoodItem.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.mehvahdjukaar.moonlight.api.client.ICustomItemRendererProvider;
 import net.mehvahdjukaar.moonlight.api.client.ItemStackRenderer;
-import net.minecraft.util.datafix.DataFixers;
+import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.minecraft.world.item.Item;
 
 import java.util.function.Supplier;
@@ -19,6 +19,6 @@ public class AllWoodItem extends Item implements ICustomItemRendererProvider {
     @Environment(EnvType.CLIENT)
     @Override
     public Supplier<ItemStackRenderer> getRendererFactory() {
-        return AllWoodItemRenderer::new;
+        return () -> new BlockTypeCycleItemRenderer<>(WoodType.class);
     }
 }
