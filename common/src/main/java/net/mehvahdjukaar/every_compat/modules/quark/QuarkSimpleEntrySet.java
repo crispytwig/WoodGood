f133c6fa77a57d2a38631056a7a4d2b23e9e43de
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-12-07 02:53:49 +0100

    a

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/quark/QuarkSimpleEntrySet.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/quark/QuarkSimpleEntrySet.java
@@ -55,6 +55,7 @@ public class QuarkSimpleEntrySet<T extends BlockType, B extends Block> extends S
         this.zetaModule = Suppliers.memoize(() -> Quark.ZETA.modules.get(m));
     }
 
+
     @Override
     public void generateRecipes(SimpleModule module, DynamicDataPack pack, ResourceManager manager) {
         ZetaModule mod = zetaModule.get();
