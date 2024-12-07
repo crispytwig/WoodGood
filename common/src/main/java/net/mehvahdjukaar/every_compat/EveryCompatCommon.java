8738d2990dd22e9c3ed5f77bb978780c84b7e27b
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-06 18:35:36 -0700

    EveryCompatCommon: Cleanup & Reorganized

--- common/src/main/java/net/mehvahdjukaar/every_compat/EveryCompatCommon.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/EveryCompatCommon.java
@@ -13,10 +13,12 @@ import net.mehvahdjukaar.every_compat.modules.friendsandfoes.FriendsAndFoesModul
 import net.mehvahdjukaar.every_compat.modules.furnish.FurnishModule;
 import net.mehvahdjukaar.every_compat.modules.handcrafted.HandcraftedModule;
 import net.mehvahdjukaar.every_compat.modules.hearth_and_home.HearthAndHomeModule;
+import net.mehvahdjukaar.every_compat.modules.lieonlion.MoreCraftingTablesModule;
 import net.mehvahdjukaar.every_compat.modules.mrcrayfish.BackpackedModule;
 import net.mehvahdjukaar.every_compat.modules.mrcrayfish.RefurbishedFurnitureModule;
 import net.mehvahdjukaar.every_compat.modules.quark.QuarkModule;
 import net.mehvahdjukaar.every_compat.modules.storagedrawers.StorageDrawersModule;
+import net.mehvahdjukaar.every_compat.modules.stylish_stiles.StylishStilesModule;
 import net.mehvahdjukaar.every_compat.modules.table_top_craft.TableTopCraftModule;
 import net.mehvahdjukaar.every_compat.modules.twigs.TwigsModule;
 import net.mehvahdjukaar.every_compat.modules.valhelsia_furniture.ValhelsiaFurnitureModule;
@@ -43,7 +45,7 @@ public class EveryCompatCommon {
 
     protected void addModules() {
 
-// ================================================= Add Other Compat Mods ========================================== \\
+//!! ============================================ Add Other Compat Mods ============================================= \\
         addOtherCompatMod("compatoplenty", "biomesoplenty", List.of("twigs", "farmersdelight", "quark", "woodworks"));
         addOtherCompatMod("compat_makeover", "biomemakeover", List.of("habitat", "farmersdelight", "quark", "decorative_blocks"));
         addOtherCompatMod("decorative_compat", "biomesoplenty", List.of("decorative_blocks"));
@@ -65,29 +67,32 @@ public class EveryCompatCommon {
         addOtherCompatMod("abnormals_delight", List.of("autumnity", "upgrade_aquatic",
                 "environmental", "atmospheric", "endergetic", "caves_and_chasms"), List.of("farmersdelight"));
 
-        // ============================================= Add Modules ==================================================== \\
+//!! =============================================== Add Modules ==================================================== \\
         addIfLoaded("another_furniture", () -> AnotherFurnitureModule::new);
-        addIfLoaded("dawnoftimebuilder", () -> DawnOfTimeModule::new);
         addIfLoaded("backpacked", () -> BackpackedModule::new);
         addIfLoaded("barkcarpets", () -> BarkCarpetsModule::new); // Exline's
         addIfLoaded("bc", () -> BeautifulCampfiresModule::new);
         addIfLoaded("campchair", () -> CampChairModule::new);
         addIfLoaded("chipped", () -> ChippedModule::new);
+        addIfLoaded("dawnoftimebuilder", () -> DawnOfTimeModule::new);
         addIfLoaded("decorative_blocks", () -> DecorativeBlocksModule::new);
+        addIfLoaded("farmersdelight", () -> FarmersDelightModule::new);
         addIfLoaded("friendsandfoes", () -> FriendsAndFoesModule::new);
         addIfLoaded("furnish", () -> FurnishModule::new);
-        addIfLoaded("hearth_and_home", () -> HearthAndHomeModule::new);
-        addIfLoaded("quark", () -> QuarkModule::new);
-        addIfLoaded("twigs", () -> TwigsModule::new);
-        addIfLoaded("refurbished_furniture", () -> RefurbishedFurnitureModule::new);
-        addIfLoaded("farmersdelight", () -> FarmersDelightModule::new);
         addIfLoaded("handcrafted", () -> HandcraftedModule::new);
+        addIfLoaded("hearth_and_home", () -> HearthAndHomeModule::new);
+        addIfLoaded("lolmct", () -> MoreCraftingTablesModule::new);
+        addIfLoaded("quark", () -> QuarkModule::new);
+        addIfLoaded("refurbished_furniture", () -> RefurbishedFurnitureModule::new);
+//        addIfLoaded("storagedrawers", () -> StorageDrawersModule::new);
+        addIfLoaded("stylishstiles", () -> StylishStilesModule::new);
+        addIfLoaded("table_top_craft", () -> TableTopCraftModule::new);
+        addIfLoaded("twigs", () -> TwigsModule::new);
         addIfLoaded("valhelsia_furniture", () -> ValhelsiaFurnitureModule::new);
         addIfLoaded("villagersplus", () -> VillagersPlusModule::new);
-        addIfLoaded("table_top_craft", () -> TableTopCraftModule::new);
-        addIfLoaded("storagedrawers", () -> StorageDrawersModule::new);
 
-// ===================================================== OTHERS ===================================================== \\
+
+//!! =================================================== OTHERS ===================================================== \\
         forAllModules(m -> EveryCompat.LOGGER.info("Loaded {}", m.toString()));
     }
 }
